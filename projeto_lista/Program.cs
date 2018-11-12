c33c3aae77421a409c5d936884603ae7e97f4950
andreivercosa <aluno@MacBook-Pro-de-Aluno.local>
2018-11-12 19:05:38 -0400

    deletar de uma lista

---
+++ projeto_lista/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace projeto_lista
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nomes = new string[3];
+            nomes[0] = "Andrei";
+            nomes[1] = "Sofia";
+            nomes[2] = "Darcy";
+
+            var listaNomes = nomes.ToList();
+            listaNomes.Remove("Darcy");
+
+            foreach(string el in listaNomes)
+            {
+                Console.WriteLine("Nome: " + el);
+            }
+
+        }
+    }
+}
