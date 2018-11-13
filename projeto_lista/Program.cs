9e761d6fe8cddee0dc55099945d79a5d88b59320
andreivercosa <aluno@MacBook-Pro-de-Aluno.local>
2018-11-12 20:16:41 -0400

    remover e add

--- projeto_lista/Program.cs
+++ projeto_lista/Program.cs
@@ -8,26 +8,34 @@ namespace projeto_lista
     {
         static void Main(string[] args)
         {
-            List<String> lista = new List<string>();
-
-            lista.Add("Maria");
-            lista.Add("Joaquina");
-            lista.Add("Raimunda");
-
-            Console.Write(lista.Count);
-
-            Console.WriteLine("Informe mais um valor par a lista");
-            lista.Add(Console.ReadLine());
-
-            Console.WriteLine("Informe o nome para de elimindo lista");
-            lista.Remove(Console.ReadLine());
+            string texto="";
+            int qtdant;
+            List<string> lista = new List<string>();
 
 
-            foreach (var e in lista){
-                Console.WriteLine("Lista: " + e);
+            do
+            {
+                Console.Write("Informe o nome a ser add: ");
+                lista.Add(Console.ReadLine().Trim());
+
+                Console.Write("Digite Sair ou der enter par continuar : ");
+                texto = Console.ReadLine();
+                Console.Clear();
+
+            } while (texto != "sair");
+
+            qtdant = lista.Count();
+
+                Console.Write("Informe o nome a ser eliminado: ");
+                lista.Remove(Console.ReadLine());
+
+            Console.Clear();
+            foreach(var l in lista){
+                Console.Write(" " + l);
             }
 
-            Console.Write(lista.Count);
+            Console.WriteLine("\nHaviam {0} numeros agora ha {1}", qtdant, lista.Count);
+
 
         }
     }
